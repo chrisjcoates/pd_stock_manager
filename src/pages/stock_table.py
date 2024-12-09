654c3fb08d641050ea835f0f9b0c626b8d83ad37
ChrisJCoates24 <chriscoates9@gmail.com>
2024-12-09 15:31:05 +0000

    created button widgets, refactored widgets into functions

--- src/pages/stock_table.py
+++ src/pages/stock_table.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QTableWidget, QTableWidgetItem
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QTableWidget, QTableWidgetItem, QHBoxLayout
 from PySide6.QtCore import Qt
 from database.database import Database
 
@@ -6,7 +6,7 @@ class StockTable(QWidget):
     def __init__(self):
         super().__init__()
 
-        page_layout = QVBoxLayout(self)
+        self.page_layout = QVBoxLayout(self)
         self._database = Database()
 
         # Set table attributes & get table data
@@ -15,14 +15,40 @@ class StockTable(QWidget):
         self.data = self._database.get_stock_data()
         self.update_row_column_count()
 
+        # Add widgets to layout
+        self.create_button_widgets()
+        self.create_table_widget()
+        self.refresh_table()
+
+        
+    def create_button_widgets(self):
+        
+        button_widget = QWidget()
+        button_layout = QHBoxLayout(button_widget)
+
+        add_button = QPushButton(text="Add")
+        edit_button = QPushButton(text="Edit")
+        delete_button = QPushButton(text="Delete")
+
+        button_layout.addWidget(add_button)
+        button_layout.addWidget(edit_button)
+        button_layout.addWidget(delete_button)
+
+        self.page_layout.addWidget(button_widget)
+        
+
+    def create_table_widget(self):
         # Create table widget
         self.table_widget = QTableWidget(self)
-        page_layout.addWidget(self.table_widget)
+        self.page_layout.addWidget(self.table_widget)
         self.table_widget.setRowCount(self._row_count)
         self.table_widget.setColumnCount(self._column_count)
         # Set table column header labels
-        self.table_widget.setHorizontalHeaderLabels(["ID", "Name", "Qty", "Supplier", "Location", "Bay"])
+        self.table_widget.setHorizontalHeaderLabels(["ID", "Name", "Qty", "Re-Order", "Supplier", "Location", "Bay"])
 
+
+    def refresh_table(self):
+        self.data = self._database.get_stock_data()
         # Add data to table
         for row_index, row_data in enumerate(self.data):
             for col_index, cell_data in enumerate(row_data):
