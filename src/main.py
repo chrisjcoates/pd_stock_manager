2a35757c5c3a629accea0ad8400b25273899cd8d
ChrisJCoates24 <chriscoates9@gmail.com>
2024-12-11 22:44:14 +0000

    add comments

--- src/main.py
+++ src/main.py
@@ -2,22 +2,35 @@ from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
 from database.database import Database
 
+
 def run_app():
+    """Runs the application"""
+
     app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec()
 
+
 def try_connect_to_db():
+    """Attempt to connect to the main database, if connection fails ask the user if
+    they want to run the application without connecting to the main database."""
+
+    # check the database connection
     database = Database().connect_to_db()
+    # if connection is found run application
     if database == True:
         run_app()
     else:
         print("Failed to connect to database.")
-        run_debug_mode = input("would you like to run the app in with out database connection? (y/n): ")
-        if run_debug_mode.lower() == 'y':
+        # ask dev if they want to run app without main database
+        run_debug_mode = input(
+            "Would you like to run the app in without the main database connection? (y/n): "
+        )
+        # run app if dev inputs y
+        if run_debug_mode.lower() == "y":
             run_app()
-            
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try_connect_to_db()
