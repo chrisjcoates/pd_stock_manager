611043b1f2cbd9af6310133929bb2d7d146b5004
ChrisJCoates24 <chriscoates9@gmail.com>
2024-12-13 12:56:37 +0000

    added close event signal

--- src/input_forms/add_product.py
+++ src/input_forms/add_product.py
@@ -7,11 +7,13 @@ from PySide6.QtWidgets import (
     QDoubleSpinBox,
     QComboBox,
 )
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
 from database.database import Database
 
 
 class AddProduct(QWidget):
+    closed_signal = Signal()
+
     def __init__(self):
         super().__init__()
 
@@ -21,6 +23,10 @@ class AddProduct(QWidget):
         self.setWindowModality(Qt.ApplicationModal)
 
         self.create_widgets()
+
+    def closeEvent(self, event):
+        self.closed_signal.emit()
+        event.accept()
 
     def create_widgets(self):
         self.name_input = QLineEdit()
@@ -113,3 +119,5 @@ class AddProduct(QWidget):
             qty=qty,
             reorder=reorder,
         )
+
+        self.destroy()
