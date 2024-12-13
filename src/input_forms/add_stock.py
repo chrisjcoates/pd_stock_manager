1c597cc97261a3c380cebc1d02b3e89f95ab5596
ChrisJCoates24 <chriscoates9@gmail.com>
2024-12-13 09:55:14 +0000

    populated location combobox

--- src/input_forms/add_stock.py
+++ src/input_forms/add_stock.py
@@ -8,6 +8,7 @@ from PySide6.QtWidgets import (
     QComboBox,
 )
 from PySide6.QtCore import Qt
+from database.database import Database
 
 
 class AddStock(QWidget):
@@ -28,12 +29,27 @@ class AddStock(QWidget):
         qty_input = QSpinBox()
         qty_input.setMinimum(0)
         qty_input.setMaximum(9999)
+
+        # Create supplier combo box
+        sup_input = QComboBox()
+
+        # Create location combo box
+        self.loc_input = QComboBox()
+        # Get locations from database
+        locations = Database().get_locations()
+        # Only get the first 2 columns (id, name)
+        locations = [row[0:2] for row in locations]
+        # Add values to combobox
+        for loc_id, loc_name in locations:
+            self.loc_input.addItem(loc_name, userData=loc_id)
+
+        # Create bay cobo box
+        bay_input = QComboBox()
+
+        # Create spin boxes
         re_order_input = QSpinBox()
         re_order_input.setMinimum(0)
         re_order_input.setMaximum(9999)
-        sup_input = QComboBox()
-        loc_input = QComboBox()
-        bay_input = QComboBox()
         price_input = QDoubleSpinBox()
         price_input.setMinimum(0)
         price_input.setMaximum(9999)
@@ -48,7 +64,7 @@ class AddStock(QWidget):
         self.page_layout.addRow("Qty: ", qty_input)
         self.page_layout.addRow("Re-Order Qty: ", re_order_input)
         self.page_layout.addRow("Supplier: ", sup_input)
-        self.page_layout.addRow("Location: ", loc_input)
+        self.page_layout.addRow("Location: ", self.loc_input)
         self.page_layout.addRow("Bay: ", bay_input)
         self.page_layout.addRow("Price: ", price_input)
         self.page_layout.addRow(submit_button)
