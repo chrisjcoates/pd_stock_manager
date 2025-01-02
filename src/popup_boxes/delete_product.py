3d9a3b8b0685a19949fdd9e2913d9171dd52448c
ChrisJCoates24 <chriscoates9@gmail.com>
2025-01-02 09:45:37 +0000

    create yes / no btn click functions

--- src/popup_boxes/delete_product.py
+++ src/popup_boxes/delete_product.py
@@ -5,8 +5,10 @@ from PySide6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QHBoxLayout,
+    QMessageBox,
 )
 from PySide6.QtCore import Qt, Signal
+from database.database import Database
 
 
 class DeletePopup(QWidget):
@@ -46,6 +48,10 @@ class DeletePopup(QWidget):
         yes_btn = QPushButton("Yes")
         no_btn = QPushButton("No")
 
+        # Bind functions to button clicks
+        yes_btn.clicked.connect(self.yes_btn_click)
+        no_btn.clicked.connect(self.no_btn_click)
+
         # Add buttons to layout
         btn_layout.addWidget(yes_btn)
         btn_layout.addWidget(no_btn)
@@ -54,13 +60,33 @@ class DeletePopup(QWidget):
         self.main_layout.addWidget(btn_widget)
 
     def yes_btn_click(self):
-        pass
+
+        # get productID
+        record = Database().get_stock_data(self.record_id)
+        prod_id = record[0][-1]
+        try:
+            Database().delete_product(stock_id=self.record_id, prod_id=prod_id)
+
+            # Create message box
+            msg = QMessageBox(self)
+            msg.setText("Product record has been deleted.")
+            msg.setWindowTitle("Message")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec()
+        except Exception as e:
+            msg = QMessageBox(self)
+            msg.setText("Error deleting record.")
+            msg.setWindowTitle("Message")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec()
+        finally:
+            self.close()
 
     def no_btn_click(self):
-        pass
-
-
-app = QApplication([])
-window = DeletePopup()
-window.show()
-app.exec()
+        # Create message box
+        msg = QMessageBox(self)
+        msg.setText("Deletion cancelled.")
+        msg.setWindowTitle("Message")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec()
+        self.close()
