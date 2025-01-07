c671dbc2627503b9bc7007fbf4db953701055b16
ChrisJCoates24 <chriscoates9@gmail.com>
2025-01-07 14:17:01 +0000

    added try except statement

--- src/pages/stock_table.py
+++ src/pages/stock_table.py
@@ -249,15 +249,17 @@ class StockTable(QWidget):
         """Opens the edit product input form
         and adds close event signal to update the table data
         """
-        # Get the id of the current selected record
-        current_record = self.current_record_selected()
-        print(current_record)
-        # Creates the product input form
-        self.add_product_form = EditProduct(current_record)
-        # Create an on close signal event to refresh the table data
-        self.add_product_form.closed_signal.connect(update_table)
-        # Open the input form
-        self.add_product_form.show()
+        try:
+            # Get the id of the current selected record
+            current_record = self.current_record_selected()
+            # Creates the product input form
+            self.add_product_form = EditProduct(current_record)
+            # Create an on close signal event to refresh the table data
+            self.add_product_form.closed_signal.connect(update_table)
+            # Open the input form
+            self.add_product_form.show()
+        except:
+            pass
 
     def delete_product(self):
 
