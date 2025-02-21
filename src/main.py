3c233da162547fa1b4861525819d071c91069d54
ChrisJCoates24 <chriscoates9@gmail.com>
2025-02-21 08:47:44 +0000

    Update main to include splash screen

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QSplashScreen, QMessageBox
+from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import Qt, QTimer, QSize
 from main_window import MainWindow
 from database.database import Database
 
@@ -7,30 +9,44 @@ def run_app():
     """Runs the application"""
 
     app = QApplication([])
-    window = MainWindow()
-    window.showMaximized()
+
+    app.setWindowIcon(QIcon("src/icon/Pendle Icon.ico"))
+
+    # Load the splash screen only when app starts
+    splash_image = QPixmap("src/logo/Pendle_Logo.png").scaled(
+        QSize(400, 300), Qt.KeepAspectRatio, Qt.SmoothTransformation
+    )
+    splash = QSplashScreen(
+        splash_image, Qt.WindowStaysOnTopHint | Qt.WindowFullscreenButtonHint
+    )
+
+    splash.showMessage(
+        "Stock App Loading...", Qt.AlignBottom | Qt.AlignCenter, Qt.black
+    )
+    splash.show()
+    splash.raise_()
+
+    # Create and show the main window after a delay
+    def show_main_window():
+
+        database_connected = Database().check_db_connection()
+        if database_connected:
+            window = MainWindow()
+            window.showMaximized()
+            splash.finish(window)
+        else:
+            connection_message = QMessageBox()
+        connection_message.setText(
+            "A connection to the database could not be established. Please check network connection. Contact Admin for help."
+        )
+        connection_message.windowTitle = "No Connecton"
+        connection_message.setStandardButtons(QMessageBox.Ok)
+        connection_message.exec()
+
+    QTimer.singleShot(3000, show_main_window)
+
     app.exec()
 
 
-def try_connect_to_db():
-    """Attempt to connect to the main database, if connection fails ask the user if
-    they want to run the application without connecting to the main database."""
-
-    # check the database connection
-    database = Database().check_db_connection()
-    # if connection is found run application
-    if database == True:
-        run_app()
-    else:
-        print("Failed to connect to database.")
-        # ask dev if they want to run app without main database
-        run_debug_mode = input(
-            "Would you like to run the app in without the main database connection? (y/n): "
-        )
-        # run app if dev inputs y
-        if run_debug_mode.lower() == "y":
-            run_app()
-
-
 if __name__ == "__main__":
-    try_connect_to_db()
+    run_app()
